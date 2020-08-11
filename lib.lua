bf11fc647b70d62857083567bf44b8271e554bda
systematicequation <56933490+systematicequation@users.noreply.github.com>
2020-08-11 14:46:07 -0700

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -19,7 +19,7 @@ local defaults; do
                             local objectPosition = Vector2.new(mouse.X - frame.AbsolutePosition.X, mouse.Y - frame.AbsolutePosition.Y);
                             while heartbeat:wait() and inputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
                                 pcall(function()
-                                    frame:TweenPosition(UDim2.new(0, mouse.X - objectPosition.X, 0, mouse.Y - objectPosition.Y), 'Out', 'Linear', 0.1, true);
+                                    frame:TweenPosition(UDim2.new(0, mouse.X - objectPosition.X, 0, mouse.Y - objectPosition.Y), 'Out', 'Linear', --[[0.1]]0, true);
                                 end)
                             end
                         end
