840b237368faa2ed8d7d83527c5d1a33be4845ce
Honor <sakamotovm@gmail.com>
2020-10-08 17:18:05 -0700

    lol

--- lib.lua
+++ lib.lua
@@ -1,3 +1,5 @@
+-- not really sure what i'm doing lol
+
 local library = {count = 0, queue = {}, callbacks = {}, rainbowtable = {}, toggled = true, binds = {}};
 local defaults; do
     local dragger = {}; do
